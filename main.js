1da2668a53e6f28eb814a6fc8fa9c1c28b419c04
Kounain Aftab <ishadoll03082@gmail.com>
2025-02-05 03:18:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("hello world");
+console.log("hello welcome back to my channel");
+console.log("kounain");
+console.log("this is the third class of it");
+console.log("alhamdulliah for everything");
+//variables are like container that store values,datatypes.
+//let
+//var "we will not use var because it don't detect errors"
+//const "we will use const when something is unchangable"
+//comments "when we don't want to run/execute these lines"
+//let message ="hello world"
+//console.log(message);
+//let message"hello world"
+var message=("hello world")
+console.log(message);
+
+var message="hello kounain"
+console.log(message);
+const author="isha aftab"
+console.log(author)
---
+++ main.ts
@@ -0,0 +1,18 @@
+console.log("hello world");
+console.log("hello welcome back to my channel");
+console.log("kounain");
+console.log("this is the third class of it");
+console.log("alhamdulliah for everything");
+//variables are like container that store values,datatypes.
+//let
+//var "we will not use var because it don't detect errors"
+//const "we will use const when something is unchangable"
+//comments "when we don't want to run/execute these lines"
+//let message ="hello world"
+//console.log(message);
+let message="hello world"
+message="hello kounain"
+
+
+
+
